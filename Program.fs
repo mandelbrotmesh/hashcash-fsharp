95598afad5298b81bd5a5fbe4678695cee9f4d14
DESKTOP-BBKBQMF\Marnee <marnee.dearman@gmail.com>
2017-08-05 19:15:36 -0700

    added function to verify the result. main will now take arguments to get the sender header or verify the sender.

--- Program.fs
+++ Program.fs
@@ -5,19 +5,16 @@ open System.Security.Cryptography
 open System.Threading
 open System.Text
 
+let sha = System.Security.Cryptography.SHA1.Create()
+
+let getHash (input:string) =
+    sha.ComputeHash(Encoding.UTF8.GetBytes(input))
+
 let seed = 
     ()
 
-let compute header counter  =
-    // printfn "SEED %s " header
-    let sha = System.Security.Cryptography.SHA1.Create()
-    let headerPlusCounter = sprintf "%s:%i" header counter
-    let hash = sha.ComputeHash(Encoding.UTF8.GetBytes(headerPlusCounter))
-    // printfn "HASH %A " hash
-    // let checkBytes = hash |> Array.take zeroBytes
-    // printfn "BYTES CHECK %A " checkBytes
-    // let shift = hash.[15] >>> 140
-    // printfn "%A" shift
+    //THIS COULD BE A USEFUL FUNCTION FOR DEBUGGIN
+    //THIS WILL PRINT THE BITS INSIDE YOUR ARRAY OF BYTES
     // let bytesToBits (bytes:byte[])  =          
     //     let bitMasks = Seq.unfold (fun bitIndex -> Some((byte(pown 2 bitIndex), bitIndex), bitIndex + 1)) 0     
     //                         |> Seq.take 8
@@ -30,61 +27,67 @@ let compute header counter  =
     //         |> List.map byteToBitArray
     //         |> List.collect id
     //         |> List.toArray
-    
-    // let bits = bytesToBits hash
-    // printfn "BITS %A" bits
-    // printfn "LENGTH %i" bits.Length
-    // let bitArray = System.Collections.BitArray(hash)
-    // printfn "bit array %A " bitArray
-    // // printfn "left shift %A" (checkBytes.[0] >>>> 2)
-    // //create a bit mask
-    // //and && the arrays together
-    // //and math magic
-    // let zeroArray = Array.fill (Array.zeroCreate zeroBytes) 0 zeroBytes 0
-    // printfn "ZERO ARRAY %A " zeroArray
-    let firstByte = (hash.[0] = byte 0)
-    // printfn "first %A " firstByte
-    let secondByte = (hash.[1] = byte 0)
-    // printfn "second %A " secondByte
-    let thirdByte = (hash.[2] <= byte 15)
-    // printfn "third %A " thirdByte
-    // printfn "byte %A" hash.[0]
-    printfn "lots of zeroes? %A" (firstByte && secondByte && thirdByte)
-    firstByte && secondByte && thirdByte
-
-    // let test = Array.init 2 (fun i -> byte(i*i))
-    // printfn "zero? %A" test
-    // printfn "is it zero? %A" (test.[0] = (byte 0))
-    // test.[0] = byte 0
-let verify =
-    ()
-
 let randomChars len = 
     //"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    let rand = System.Random() //RandomNumberGenerator.Create()
+    let rand = System.Random() 
     let chars = String(Array.concat [ [|'A'..'Z'|]; [|'0'..'9'|]; [|'a'..'b'|] ])
     String(Array.init len (fun _ -> chars.[rand.Next(chars.Length)]))
 
-type headerParts =
-    {
-        version: string
-        bits: string
-        timestamp: string
-        resource: string
-        extension: string
-        seed: string
-        counter: string
-    }
+let mutable counter = 0
+
+let header counter randomString =
+    let ver = sprintf "%i" 1
+    let bits = sprintf "%i" 20
+    let date = sprintf "%s" (DateTime.Now.ToString("yyMMddmmss"))
+    let resource = sprintf "%s" "hashcash@mailinator.com"
+    let extension = sprintf "%s" String.Empty
+    let rand = sprintf "%s" randomString 
+    sprintf "%s:%s:%s:%s:%s:%s:%i" ver bits date resource extension rand counter
+
+let checkHash (hash:byte[]) =
+    let firstByte = (hash.[0] = byte 0)
+    let secondByte = (hash.[1] = byte 0)
+    let thirdByte = (hash.[2] <= byte 15)
+    firstByte && secondByte && thirdByte
+
+let compute index randomString = 
+    let hash = getHash (header index randomString)
+    if not <| checkHash hash 
+    then 
+        false
+    else
+        counter <- index
+        true
+
+let verify header =
+    let hash = getHash header
+    checkHash hash
+
+let sender () =
+    printfn "STARTING HASHCASH F#"
+    let seed = randomChars 8
+    let continueTaking result = 
+        not <| result        
+    let randomString = randomChars 8
+    Seq.initInfinite (fun index -> index + 1)
+    |> Seq.takeWhile (fun index -> (continueTaking (compute index randomString)
+                                   && index < 2000000)) //limit so it does not run forever just in case. this should find a solution on average in 2^20 tries
+    |> Seq.iter ignore 
+    printfn "FINAL COUNT %i " counter
+    printfn "HASH %s" (header counter randomString)
+    ()
+    
+let recipient header =
+    printfn "does it pass? %A" (verify header)
+    ()
 
 [<EntryPoint>]
 let main argv =
-    printfn "START HASHCASH F#!"
-    let dateStamp = DateTime.Now.ToString("yyMMddmmss")
-    let seed = randomChars 10
-    let continueTaking result = 
-        not <| result
-    Seq.initInfinite (fun index -> index + 1)
-    // |> Seq.takeWhile (fun x -> x < 10)
-    |> Seq.takeWhile (fun cnt -> continueTaking((compute seed cnt)) && cnt < 1000000)
-    |> Seq.iter (printfn "%i")
+    printfn "%A" DateTime.Now
+    match argv.[0] with
+    | "sender" ->       sender ()
+    | "recipient" ->    printfn "%s" argv.[1]
+                        recipient argv.[1]
+    | _ ->              printfn "%s" "Argument not allowed."
+    printfn "%A" DateTime.Now
     0 // return an integer exit code
